74e21f8559542d9c1fdd4c815f9bcb74f2e60a86
Lucas Lamar <lucaslamar@Coles-MBP.attlocal.net>
2025-02-11 18:50:05 -0600

    disabled linting during builds

---
+++ src/styles/theme.ts
@@ -0,0 +1,11 @@
+const theme = {
+    colors: {
+      primary: "#1E3A8A", // Blue
+      secondary: "#FACC15", // Yellow
+      background: "#111827", // Dark background
+      text: "#F3F4F6", // Light text
+    },
+  };
+  
+  export default theme;
+  
